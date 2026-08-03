422bb67ec2d9567d5f10aa1b0eb5e1fc27c54f05
Madhurjya Roy <m@mroy.me>
2026-08-03 23:20:24 +0530

    feat: personalize site and add email post

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Madhurjya Roy";
+export const SITE_DESCRIPTION =
+  "Personal website of Madhurjya Roy, featuring notes on software, systems, developer tooling, Linux, homelab projects, and other interests.";
